8d712b06256d4fa300177fc18ae1fb6c3f87bab7
Andrei Kozel <kozel.andrei.94@gmail.com>
2024-03-29 11:51:14 +0100

    Add Sources method to SourcePostgresStorage

--- internal/storage/source.go
+++ internal/storage/source.go
@@ -13,6 +13,25 @@ type SourcePostgresStorage struct {
 	db *sqlx.DB
 }
 
+func (s *SourcePostgresStorage) Sources(ctx context.Context) ([]model.Source, error) {
+	conn, err := s.db.Connx(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	var sources []dbSource
+	if err := conn.SelectContext(ctx, &sources, `SELECT * FROM sources`); err != nil {
+		return nil, err
+	}
+
+	return lo.Map(sources, func(source dbSource, _ int) model.Source { return model.Source(source) }), nil
+}
+
+func NewSourceStorage(db *sqlx.DB) *SourcePostgresStorage {
+	return &SourcePostgresStorage{db: db}
+}
+
 type dbSource struct {
 	ID        int64     `db:"id"`
 	Name      string    `db:"name"`
